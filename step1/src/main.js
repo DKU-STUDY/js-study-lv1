5917d75aef697cc8dd1956e16f840d1605c70603
junilHwang <junil.h@kakao.com>
2021-06-21 23:33:51 +0900

    feat: 아이템 수정/삭제 완료

--- step1/src/main.js
+++ step1/src/main.js
@@ -4,6 +4,7 @@ const state = {
     { id: 2, content: '포동이', isComplete: true, createdAt: Date.now() },
     { id: 3, content: '코코', isComplete: false, createdAt: Date.now() },
   ],
+  selectedItem: -1,
 }
 
 function template () {
@@ -23,33 +24,49 @@ function template () {
       <ul>
         
         <!-- 완료된 아이템 -->
-        ${state.todoItems.map(function (item) {
+        ${state.todoItems.map(function (item, key) {
+          if (key === state.selectedItem) {
+            return `
+              <li>
+                <form name="modifierForm" action="">
+                  <fieldset>
+                    <legend hidden>아이템 수정</legend>
+                    <label>
+                      <span hidden>아이템 수정</span>
+                      <input type="text" value="${item.content}" size="40">
+                    </label>
+                    <button type="submit">완료</button>
+                    <button type="button">취소</button>
+                  </fieldset>
+                </form>
+              </li>
+              <!-- / 수정 중인 아이템 -->
+            `
+          }
           return `
             <li>
               <p>${item.content}</p>
               <button type="button">취소</button>
-              <button type="button">수정</button>
-              <button type="button">삭제</button>
+              
+              <button
+                type="button"
+                class="modifier"
+                data-key="${key}"
+              >
+                수정
+              </button>
+              
+              <button
+                type="button"
+                class="deleter"
+                data-key="${key}"
+              >
+                삭제
+              </button>
             </li>
           `
         }).join('')}
         <!-- / 완료된 아이템 -->
-  
-        <!-- 수정 중인 아이템 -->
-        <li>
-          <form name="modifierForm" action="">
-            <fieldset>
-              <legend hidden>아이템 수정</legend>
-              <label>
-                <span hidden>아이템 수정</span>
-                <input type="text" value="2주차: Webpack + TodoList 리팩토링" size="40">
-              </label>
-              <button type="submit">완료</button>
-              <button type="button">취소</button>
-            </fieldset>
-          </form>
-        </li>
-        <!-- / 수정 중인 아이템 -->
       </ul>
     </main>
   `
@@ -63,11 +80,18 @@ function render () {
   const $app = document.querySelector('#app');
   $app.innerHTML = template();
 
-  // 이벤트 등록
-  const $form = $app.querySelector('form[name="appenderForm"]');
-  const itemAdd = function (event) {
+  // 태그 등록
+  const $appenderForm = $app.querySelector('form[name="appenderForm"]');
+  const $modifiers = $app.querySelectorAll('.modifier');
+  const $deleter = $app.querySelectorAll('.deleter');
+  const $modifierForm = $app.querySelector('form[name="modifierForm"]');
+
+  /** 이벤트 등록 **/
+
+  // 아이템 추가 관리
+  const addItem = function (event) {
     event.preventDefault();
-    const content = $form.querySelector('input').value.trim();
+    const content = $appenderForm.querySelector('input').value.trim();
     if (content.length === 0) {
       return alert('아이템 내용을 입력해주세요');
     }
@@ -80,7 +104,44 @@ function render () {
     });
     render();
   }
-  $form.addEventListener('submit', itemAdd);
+  $appenderForm.addEventListener('submit', addItem);
+
+  // 아이템 수정 버튼 관리
+  const editItem = function (event) {
+    state.selectedItem = Number(event.target.dataset.key);
+    render();
+  }
+  $modifiers.forEach(function ($modifier) {
+    $modifier.addEventListener('click', editItem)
+  })
+
+  // 아이템 수정 관리
+  const updateItem = function (event) {
+    event.preventDefault();
+    const content = event.target.querySelector('input').value.trim();
+    if (content.length === 0) {
+      return alert('아이템 내용을 입력해주세요');
+    }
+    state.todoItems[state.selectedItem].content = content;
+    state.selectedItem = -1;
+    render();
+  }
+  if ($modifierForm) {
+    $modifierForm.addEventListener('submit', updateItem);
+  }
+
+  // 아이템 삭제 관리
+  const deleteItem = function (event) {
+    const key = Number(event.target.dataset.key);
+    state.todoItems.splice(key, 1);
+    render();
+  }
+  $deleter.forEach(function ($el) {
+    $el.addEventListener('click', deleteItem)
+  })
+
+  /** 이벤트 등록 **/
+
 }
 
 function main () {
