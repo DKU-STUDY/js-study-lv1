deb4ed6a2bb7b236ef8ec8e11d0bb4f9d1e6934c
himitery <himit0131@naver.com>
2021-06-27 20:38:47 +0900

    feat: step1 - 입력한 내용이 없을 때 경고창 - 수정 input에 escape, enter event 추가

--- step1/src/main.js
+++ step1/src/main.js
@@ -44,7 +44,7 @@ const htmlTemplate = () => `
               <form name="updateItem" method="post" data-id="${item.id}">
                 <fieldset>
                   <label>
-                    <input type="text" value="${item.content}" />
+                    <input class="editInput" type="text" value="${item.content}" data-id="${item.id}"/>
                   </label>
                   <button class="update" type="submit" data-id="${item.id}">완료</button>
                   <button class="cancel" type="button" data-id="${item.id}">취소</button>
@@ -85,11 +85,16 @@ const render = () => {
   const $doneItem = document.querySelectorAll(`.done`);
   const $editItem = document.querySelectorAll(`.edit`);
   const $cancelItem = document.querySelectorAll(`.cancel`);
+  const $editInput = document.querySelectorAll(`.editInput`);
 
   const addItem = (event) => {
     event.preventDefault();
     const content = $addItemForm.querySelector('input').value.trim();
-    if (content.length === 0) return;
+    if (content.length === 0) {
+      alert('아이템 이름을 입력해주세요.');
+
+      return;
+    }
 
     const newItem = {
       id: todoList.todoItems.length === 0 ? 0 : todoList.todoItems[todoList.todoItems.length - 1].id + 1,
@@ -120,9 +125,9 @@ const render = () => {
   };
   $editItem.forEach((item) => item.addEventListener('click', editItem));
 
-  const updateItem = (event) => {
+  const updateItem = (event, target) => {
     event.preventDefault();
-    const targetId = +event.target.dataset.id;
+    const targetId = target !== undefined ? target : +event?.target?.dataset?.id;
     const content = Array.prototype.slice
       .call($updateItemForm)
       .filter((item) => item.dataset.id === String(targetId))[0]
@@ -135,13 +140,25 @@ const render = () => {
   };
   $updateItemForm.forEach((item) => item.addEventListener('submit', updateItem));
 
-  const cancelItem = (event) => {
-    const targetId = +event.target.dataset.id;
+  const cancelItem = (event, target) => {
+    const targetId = target !== undefined ? target : +event?.target?.dataset?.id;
 
     todoList.todoItems.filter((item) => item.id === targetId)[0].status = todoList.status.process;
     render();
   };
   $cancelItem.forEach((item) => item.addEventListener('click', cancelItem));
+
+  const editInputKeyEvent = (event) => {
+    if (event.key === 'Escape') {
+      const targetId = +event.target.dataset.id;
+      cancelItem(event, targetId);
+    }
+    if (event.key === 'Enter') {
+      const targetId = +event.target.dataset.id;
+      updateItem(event, targetId);
+    }
+  };
+  $editInput.forEach((item) => item.addEventListener('keydown', editInputKeyEvent));
 
   const deleteItem = (event) => {
     event.preventDefault();
