cdd3ab99634b36b46acbed0ed62788ffb7fc7218
inust33 <inust33@gmail.com>
2021-07-05 00:39:11 +0900

    change

--- step1/src/main.js
+++ step1/src/main.js
@@ -1,17 +1,42 @@
 const state = {
   todoItems: [
     { id: 1, content: 'first item', isComplete: false, createdAt: Date.now() },
-    { id: 1, content: 'second item', isComplete: false, createdAt: Date.now() },
-    { id: 1, content: 'third item', isComplete: false, createdAt: Date.now() },
-    { id: 1, content: 'fourth item', isComplete: false, createdAt: Date.now() },
-    { id: 1, content: 'fifth item', isComplete: false, createdAt: Date.now() },
+    { id: 2, content: 'second item', isComplete: false, createdAt: Date.now() },
+    { id: 3, content: 'third item', isComplete: false, createdAt: Date.now() },
+    { id: 4, content: 'fourth item', isComplete: false, createdAt: Date.now() },
+    { id: 5, content: 'fifth item', isComplete: false, createdAt: Date.now() },
   ],
+  selectedItem:0
 }
 
-function template() {
-  return `
+const itemTemplate = ({content, id, isComplete, highlight}) =>
   
-   <h1>📃 TodoList</h1>
+  
+  ${todoItems.items.map(({ content, id, isComplete, highlight }) => `
+    <li>
+      ${todoItems.selectedItem === item.id ? `
+      <form name="modifierForm" action="" id=${id}>
+      <fieldset>
+        <legend hidden>아이템 수정</legend>
+        <label>
+          <span hidden>아이템 수정</span>
+          <input type="text" value=${content} size="40">
+        </label>
+        <button type="submit">완료</button>
+        <button type="button">취소</button>
+      </fieldset>
+    </form>`: `
+      <p ${item.highlight ? `style="color: #09F"`:``}>${content}</p>
+      <button type="button">취소</button>
+      <button type="button" class="modifier" data-key="${key}">수정</button>
+      <button type="button" class="deleter" data-key="${key}">삭제</button>
+    </li>`;
+  }).join('')}
+
+
+
+  
+   `<h1>📃 TodoList</h1>
    <form name="appenderForm" action="" method="post">
      <fieldset>
        <legend hidden>TodoList Form</legend>
@@ -22,94 +47,121 @@ function template() {
        <button type="submit">전송</button>
      </fieldset>
    </form>
-   <ul>
-   ${state.todoItems.map(function (item) {
+   <ul> 
+   ${state.todoItems.map(function (item, key) {
+     if (key === state.selectedItem) {
+       return`    <!-- 수정 중인 아이템 -->
+       <li>
+         <form name="modifierForm" action="">
+           <fieldset>
+             <legend hidden>아이템 수정</legend>
+             <label>
+               <span hidden>아이템 수정</span>
+               <input type="text" value=${item.content} size="40">
+             </label>
+             <button type="submit">완료</button>
+             <button type="button">취소</button>
+           </fieldset>
+         </form>
+       </li>
+       <!-- / 수정 중인 아이템 -->`
+     }
     return `
     <li>
       <p style="color: #09F">${item.content}</p>
       <button type="button">취소</button>
-      <button type="button">수정</button>
-      <button type="button">삭제</button>
+      <button type="button" class="modifier" data-key="${key}">수정</button>
+      <button type="button" class="deleter" data-key="${key}">삭제</button>
     </li>`;
   }).join('')}
-     <!-- 완료된 아이템 -->
-     <li>
-       <p style="color: #09F">
-         1주차: Todo List 만들기
-       </p>
-       <button type="button">취소</button>
-       <button type="button">수정</button>
-       <button type="button">삭제</button>
-     </li>
-     <!-- / 완료된 아이템 -->
-
-     <!-- 수정 중인 아이템 -->
-     <li>
-       <form name="modifierForm" action="">
-         <fieldset>
-           <legend hidden>아이템 수정</legend>
-           <label>
-             <span hidden>아이템 수정</span>
-             <input type="text" value="2주차: Webpack + TodoList 리팩토링" size="40">
-           </label>
-           <button type="submit">완료</button>
-           <button type="button">취소</button>
-         </fieldset>
-       </form>
-     </li>
-     <!-- / 수정 중인 아이템 -->
-
-     <!-- 아이템의 기본 상태들 -->
-     <li>
-       <p>
-         3주차: ExpressJS로 API 서버 만들기
-       </p>
-       <button type="button">완료</button>
-       <button type="button">수정</button>
-       <button type="button">삭제</button>
-     </li>
-     <li>
-       <p>
-         4주차: API 연동
-       </p>
-       <button type="button">완료</button>
-       <button type="button">수정</button>
-       <button type="button">삭제</button>
-     </li>
-     <li>
-       <p>
-         5주차: 배포하기
-       </p>
-       <button type="button">완료</button>
-       <button type="button">수정</button>
-       <button type="button">삭제</button>
-     </li>
-     <!-- / 아이템의 기본 상태들 -->
-   </ul>`
+   
+  </main>`
 }
 
 /**
- * AppenderForm에서 submit 이벤트가 발생했을 때 처리하는 함수
- * @param event
- */
-function handleSubmitAppenderForm(event) {
-  event.preventDefault();
-  const input = event.target.querySelector('input');
-  alert(`input에 입력된 텍스트: ${input.value}`);
-  alert(`input에 입력된 텍스트의 길이: ${input.value.length}`);
-}
 
 /**
  * 앱 시작시 실행될 entry 함수
  */
-function main() {
-  const $app = document.querySelector("#app")
-  $app.innerHTML = template();
-  const $form = $app.querySelector('form[name="appenderForm"]');
+ //아이템 추가 영역
+ const itemAdd = function (event) {
 
-  // 추가 폼 전송 이벤트 등록
-  document.forms.appenderForm.addEventListener('submit', handleSubmitAppenderForm);
+  event.preventDefault();
+  const content = $appender.querySelector('input').value.trim();
+  if (content.length === 0) {
+  return alert("아이템 내용을 입력해주세요");
+  }
+  state.todoItems.push({
+    id: 4,
+    createdAt: Date.now(),
+    content: content,
+    isComplete: False
+  });
+  render();
+}
+//아이템 수정 버튼 관리
+
+const editItem = function (event) {
+  state.selectedItem = Number(event.target.dataset.key);
+  render();
+}
+//아이템 수정
+const updateItem = function (event) {
+  event.preventDefault();
+  const content = $appender.querySelector('input').value.trim();
+  if (content.length === 0) {
+  return alert("아이템 내용을 입력해주세요");
+  }
+  state.todoItems[state.selectedItem].content = content;
+  state.selectedItem = -1;
+  render();
+}
+const deleteItem = function (event) {
+  const key = Number(event.target.dataset.key);
+  state.todoItems.splice(key, 1);
+  render();
+}
+const resetItem = function(event){
+  event.preventDefault();
+  state.selectedItem = -1;
+  render();
 }
 
+
+function render() {
+  const $app = document.querySelector("#app")
+  $app.innerHTML = template();
+  //이벤트 등록
+  const $appenderForm = $app.querySelector('form[name="appenderForm"]');
+  const $modifiers = $app.querySelectorAll('.modifier');
+  const $modifierForm = $app.querySelector('form[name="modifierForm"]');
+  const $deleters = $app.querySelectorAll(".deleter");
+  const $complete = $app.querySelectorAll(".complete");
+  /** 이벤트 등록 **/
+   
+  $appender.addEventListener('submit', itemAdd);
+  
+  // 아이템 수정 관리
+  $modifiers.forEach(function ($modifier) {
+    $modifier.addEventListener("click", editItem)
+  })
+
+  
+
+  if ($modifierForm) {
+    $modifierForm.addEventListener('reset', resetItem);
+    $modifierForm.addEventListener('keydown', (event) => {if(event.code ==='Escape') {state.selectedItem=-1; render();}});
+  }
+  //아이템 삭제 관리//
+
+  $deleters.forEach(function ($deleter) {
+    $deleter.addEventListener("click", deleteItem)
+  });
+
+}
 // 앱 실행
-main();
+function main(){
+  render();
+}
+
+  main();
--- step2/README.md
+++ step2/README.md
@@ -39,6 +39,7 @@
     - [ ] Parcel, Webpack, Rollup, Vite 중 택 1
 
 ## 파일 분리 예시
+
 - [Vanilla Javascript로 컴포넌트 만들기](https://junilhwang.github.io/TIL/Javascript/Design/Vanilla-JS-Component/)
 - 사실 위의 포스트만 볼 경우 굉장히 혼란스러울 수 있다.
 - 이 부분은 온라인 세션에서 설명할 예정
@@ -46,6 +47,7 @@
 ## 코딩 규칙 예시
 
 참고링크
+
 - [객체지향 생활 체조 학습하기](https://7942yongdae.tistory.com/8)
 - [[Java] 객체지향 생활 체조 원칙 9가지 (from 소트웍스 앤솔러지)](https://jamie95.tistory.com/99)
 
@@ -71,6 +73,7 @@ function 단웅이_10번씩_5줄_반복() {
 ```
 
 위의 코드를 다음과 같이 표현할 수 있다.
+
 ```js
 function 단웅이_10번씩_5줄_반복() {
   const raw = 10;
@@ -118,22 +121,25 @@ function 단웅이_반복(repeat) {
 
 - 조건문은 복제의 원인이 되기도 하며 가독성도 좋지 않다.
 - 참고링크: https://woowacourse.github.io/javable/post/2020-07-29-dont-use-else/
+
 ```js
 function 단웅이의_일과(hour, isStudy) {
-    let status = "";
-    if (hour > 4 && hour <= 12) {
-        status = "취침";
+  let status = "";
+  if (hour > 4 && hour <= 12) {
+    status = "취침";
+  } else {
+    if (isStudy) {
+      status = "공부";
     } else {
-        if (isStudy) {
-            status = "공부";
-        } else {
-            status = "여가";
-        }
+      status = "여가";
     }
-    return status;
+  }
+  return status;
 }
 ```
+
 위의 코드는 다음과 같이 표현할 수 있다.
+
 ```js
 function 단웅이의_일과(hour, isStudy) {
   if (hour > 4 && hour <= 12) {
@@ -145,21 +151,24 @@ function 단웅이의_일과(hour, isStudy) {
   return "여가";
 }
 ```
+
 다시 다음과 같이 표현할 수 있다.
+
 ```js
 function 단웅이의_일과(hour, isStudy) {
-    if (hour > 4 && hour <= 12) {
-      return "취침";
-    }
-    return isStudy ? "공부" : "여가";
+  if (hour > 4 && hour <= 12) {
+    return "취침";
+  }
+  return isStudy ? "공부" : "여가";
 }
 ```
+
 이것도 다시 이렇게 표현할 수 있다.
+
 ```js
 function 단웅이의_일과(시간, 공부중) {
-    const 취침시간 = 4 < hour && hour <= 12;
-    return 취침시간 ? '취침' :
-           공부중   ? '공부' : '여가';
+  const 취침시간 = 4 < hour && hour <= 12;
+  return 취침시간 ? "취침" : 공부중 ? "공부" : "여가";
 }
 ```
 
@@ -185,10 +194,10 @@ function 단웅이의_일과(hour, isStudy) {
 
 ```js
 const 단웅이의_상태 = {
-  "취침": 취침,
-  "공부": 공부,
-  "여가": 여가,
-}
+  취침: 취침,
+  공부: 공부,
+  여가: 여가,
+};
 
 function 단웅이의_일과(hour, isStudy) {
   if (hour > 4 && hour <= 12) {
@@ -200,17 +209,16 @@ function 단웅이의_일과(hour, isStudy) {
 
 여기서 `4`와 `12`도 상수로 만들 수 있다.
 
-
 ```js
 const 단웅이의_상태 = {
-  "취침": 취침,
-  "공부": 공부,
-  "여가": 여가,
-}
+  취침: 취침,
+  공부: 공부,
+  여가: 여가,
+};
 const 취침_시간 = {
   시작: 4,
   끝: 12,
-}
+};
 
 function 단웅이의_일과(hour, isStudy) {
   if (취침_시간.시작 < hour && hour <= 취침_시간.끝) {
@@ -228,17 +236,23 @@ function 단웅이의_일과(hour, isStudy) {
 
 ```js
 function 랜덤_숫자_100개_만들기() {
-  return Array(100).fill(0).map(() => Math.random()).map(v => v * 100).map(Math.round);
+  return Array(100)
+    .fill(0)
+    .map(() => Math.random())
+    .map((v) => v * 100)
+    .map(Math.round);
 }
 ```
+
 위에 처럼 된 코드를 다음과 같이 표현합시다.
 
 ```js
 function 랜덤_숫자_100개_만들기() {
-  return Array(100).fill(0)
-                   .map(() => Math.random())
-                   .map(v => v * 100)
-                   .map(Math.round);
+  return Array(100)
+    .fill(0)
+    .map(() => Math.random())
+    .map((v) => v * 100)
+    .map(Math.round);
 }
 ```
 
@@ -247,22 +261,69 @@ function 랜덤_숫자_100개_만들기() {
 ```js
 function 랜덤_숫자_100개_만들기() {
   return Array(100)
-          .fill(0)
-          .map(() => Math.random())
-          .map(v => v * 100)
-          .map(Math.round);
+    .fill(0)
+    .map(() => Math.random())
+    .map((v) => v * 100)
+    .map(Math.round);
 }
 ```
-
 
 ### 줄여쓰지 않는다 (축약 금지)
 
 - 길게 설명하지 않겠다.
 - 아니, 내가 설명하지 않겠다.
-- [이 포스트](https://velog.io/@mowinckel/%EB%88%84%EA%B0%80-%EC%9D%B4%EB%A6%84%EC%9D%84-%ED%95%A8%EB%B6%80%EB%A1%9C-%EC%A7%93%EB%8A%94%EA%B0%80)로 대체한다. 
+- [이 포스트](https://velog.io/@mowinckel/%EB%88%84%EA%B0%80-%EC%9D%B4%EB%A6%84%EC%9D%84-%ED%95%A8%EB%B6%80%EB%A1%9C-%EC%A7%93%EB%8A%94%EA%B0%80)로 대체한다.
 
 ## 번들러 관련 자료
+
 - [모듈화의 역사](https://medium.com/@chullino/%EC%9B%B9%ED%8C%A9-3-4-js%EB%AA%A8%EB%93%88%ED%99%94-%EC%97%AD%EC%82%AC-%EB%8F%8C%EC%95%84%EB%B3%B4%EA%B8%B0-1-9df997f82002)
 - [module](https://gitlab.com/siots-study/topics/-/wikis/module)
 - [모듈 소개](https://ko.javascript.info/modules-intro)
 - [babel과 webpack을 이용한 ES6 환경 구축](https://poiemaweb.com/es6-babel-webpack-1)
+
+- [ ] 리팩토링
+  - [ ] ECMAScript에 대한 조사
+    - [ ] ECMAScript, Javascript 용어 정리
+    - [ ] ES5 vs ES6 차이점 정리
+  - [ ] 파일을 기능 단위로 분리해본다.
+    - [ ] core: 어플리케이션의 베이스 코드
+    - [ ] components: 컴포넌트 코드
+    - [ ] utils: 유틸리티 성향의 코드
+    - [ ] constants: 상수
+    - [ ] app.js (entry point)
+  - [ ] 다음과 같은 규칙을 지켜가며 코딩한다.
+    - [ ] 한 메소드(함수)에 indent(tab)는 최대 2depth로 유지하기
+    - [ ] else 예약어(keyword)를 쓰지 않는다.
+    - [ ] 상수를 적극적으로 사용한다.
+    - [ ] 한 줄에 점을 하나만 찍는다.
+    - [ ] 줄여쓰지 않는다 (축약 금지)
+- [ ] 번들러 조사 및 적용
+  - [ ] 번들러에 대해 알아보기
+    - [ ] javascript 번들링
+    - [ ] 번들링을 하는 이유, 필요한 이유
+    - [ ] 번들러로 할 수 있는 일들
+  - [ ] 번들러 종류 알아보기
+    - [ ] parcel
+    - [ ] webpack
+    - [ ] rollup
+    - [ ] vite
+  - [ ] 모듈 시스템에 대해 알아보기
+    - [ ] CommonJS
+    - [ ] AMD
+    - [ ] RequireJS
+    - [ ] ESM
+  - [ ] 브라우저 모듈에 대해 알아보기
+  - [ ] 번들러 적용
+    - [ ] 번들러 설치를 위해 nodejs + npm 설치
+    - [ ] Parcel, Webpack, Rollup, Vite 중 택 1
+
+## ECMAscript, JAVAscript
+
+- ECMA-262는 Ecma 인터내셔널에 의해 제정된 하나의 기술 규격의 이름으로, 범용 목적의 스크립트 언어에 대한 명세를 담고 있다.
+- 스크립트 언어는 독립된 시스템에서 작동하도록 특별히 설계된 프로그래밍 언어로, 응용 프로그램과는 독립적이며, 사용자가 직접 프로그램을 의도에 따라 동작시킬 수 있다는 특징이 있다.
+- ECMAscript란 ECMA-262 규격에 의해 정의된 범용 스크립트 언어를 말하며, JAVAscript은 이에 포함된다.
+
+## ES5, ES6
+
+- ECMAscript가 배포된 버전이 ES5, ES6 등이며 ES6부터는 숫자 대신 연도를 붙여 ECMA2015(=ES6)같은 명칭을 사용한다.
+- ES5에서 ES6로 넘어오면서 let, const, Class, Arrow function 및 module import/export 등이 도입되었다.
