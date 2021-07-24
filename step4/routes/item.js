cbd397222c4237cec696fa7bbf617f1fc4b58cc4
kykapple <kykapple@naver.com>
2021-07-24 23:25:27 +0900

    catch error

--- step4/routes/item.js
+++ step4/routes/item.js
@@ -3,52 +3,70 @@ const itemsRepository = require("../repository/ItemRepository.js");
 
 const router = express.Router();
 
-router.get('/', (request, response) => {
-    itemsRepository.getItemList()
-        .then(itemList => response.json(itemList));
+router.get('/', (request, response, next) => {
+    itemsRepository
+        .getItemList()
+        .then(itemList => response.json(itemList))
+        .catch(error => next(error));
 });
 
-router.post('/', (request, response) => {
-    itemsRepository.insertItem([
-        request.body.content,
-        request.body.highlight,
-        request.body.isComplete,
-        request.body.selected,
-    ]).then(() => response.json({ message: 'success' }));
+router.post('/', (request, response, next) => {
+    itemsRepository
+        .insertItem([
+            request.body.content,
+            request.body.highlight,
+            request.body.isComplete,
+            request.body.selected,
+        ])
+        .then(() => response.json({ message: 'success' }))
+        .catch(error => next(error));
 });
 
-router.delete('/', (request, response) => {
-    itemsRepository.deleteItem([ request.body.id ] )
-        .then(() => response.json({ message: 'success' }));
+router.delete('/', (request, response, next) => {
+    itemsRepository
+        .deleteItem([ request.body.id ] )
+        .then(() => response.json({ message: 'success' }))
+        .catch(error => next(error));
 });
 
-router.put('/:id', (request, response) => {
-    itemsRepository.getItem([ request.params.id ])
+router.put('/:id', (request, response, next) => {
+    itemsRepository
+        .getItem([ request.params.id ])
         .then(item => {
-            itemsRepository.selectItem([ !item.selected, item.id ])
+            itemsRepository
+                .selectItem([ !item.selected, item.id ])
                 .then(() => response.json({ message: 'success'}))
-        });
+        })
+        .catch(error => next(error));
 })
 
-router.put('/', (request, response) => {
-    itemsRepository.modifyItem([ request.body.content, request.body.id ])
-        .then(() => response.json({ message: 'success' }));
+router.put('/', (request, response, next) => {
+    itemsRepository
+        .modifyItem([ request.body.content, request.body.id ])
+        .then(() => response.json({ message: 'success' }))
+        .catch(error => next(error));
 });
 
-router.put('/complete/:id', (request, response) => {
-    itemsRepository.getItem([ request.params.id ])
+router.put('/complete/:id', (request, response, next) => {
+    itemsRepository
+        .getItem([ request.params.id ])
         .then(item => {
-            itemsRepository.completeItem([ !item.isComplete, item.id ])
+            itemsRepository
+                .completeItem([ !item.isComplete, item.id ])
                 .then(() => response.json({ message: 'success'}))
-        });
+        })
+        .catch(error => next(error));
 });
 
-router.put('/toggle/:id', (request, response) => {
-    itemsRepository.getItem([ request.params.id ])
+router.put('/toggle/:id', (request, response, next) => {
+    itemsRepository
+        .getItem([ request.params.id ])
         .then(item => {
-            itemsRepository.toggleItem([ !item.highlight, item.id ])
+            itemsRepository
+                .toggleItem([ !item.highlight, item.id ])
                 .then(() => response.json({ message: 'success'}))
-        });
+        })
+        .catch(error => next(error));
 });
 
 module.exports = router;
