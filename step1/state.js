55b1c9ca12ea799a9b225756fc0800d08af5187e
JunilHwang <junil.h@kakao.com>
2021-10-10 23:12:06 +0900

    docs: 전체적인 미션 내용 업데이트 (2)

--- step1/state.js
+++
@@ -1,8 +0,0 @@
-export const state = {
-    todoItems : [
-        {id: 1, content: '첫번째 아이템', isComplete: false, createdAt:Date.now()},
-        {id: 2, content: '두번째 아이템', isComplete: false, createdAt:Date.now()},
-        {id: 3, content: '세번째 아이템', isComplete: false, createdAt:Date.now()},
-    ],
-    selectedItem: -1,
-}
