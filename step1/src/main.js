3ab041fcccd556211ccca56de028c90a1f47e0b6
Jang Min Sung <mstoto0319@gmail.com>
2021-10-14 21:06:39 +0900

    feat: {step1} 아이템 추가 기능 작성 완료

--- step1/src/main.js
+++ step1/src/main.js
@@ -1,1 +1,32 @@
 // 코드 작성
+// 3.태그 추가 함수
+function appendItem (event){
+    //고유 이벤트 제거
+    event.preventDefault();
+
+    //태그 선택
+    var $appender = $appenderForm.querySelector('input');
+
+    //태그 생성
+    var $newItem = document.createElement('li');
+    
+    //태그의 내용을 채워넣기
+    $newItem.innerHTML = `
+        <p>
+          ${$appender.value}
+        </p>
+        <button type="button">취소</button>
+        <button type="button">수정</button>
+        <button type="button">삭제</button>
+    `;
+    document.getElementById('todoList').appendChild($newItem);
+    $appender.value = ' ';
+    $appender.focus();
+}
+//1. DOM 접근
+var $appenderForm = document.forms.appenderForm;
+
+//2. 이벤트 등록
+$appenderForm.onsubmit = appendItem;
+    
+}
