ec74f22cc7ca9d69072134adcf095ba77e1a72c6
eyabc <bey4314@naver.com>
2021-10-24 22:02:26 +0900

    feat: webpack env production

--- step1/src/index.js
+++ step1/src/index.js
@@ -2,13 +2,6 @@ import applyAllEvents from './events.js';
 import data from './data/index.js';
 import ItemListEl from './elements/ItemListEl.js';
 import 'Static/style.css';
-
-import TestImage from 'Static/temp.png';
-import Data from 'Static/data.xml';
-import Notes from 'Static/data.csv';
-import toml from 'Static/data.toml';
-import yaml from 'Static/data.yaml';
-import json from 'Static/data.json5';
 import Print from './print';
 
 const $body = document.querySelector('body');
@@ -48,24 +41,6 @@ const execute = () => {
   applyAllEvents();
 }
 execute();
-
-
-(function testWebpackLoader() {
-  const testImage = new Image();
-  testImage.src = TestImage;
-  $body.appendChild(testImage);
-
-  console.log({ Data });
-  console.log({ Notes });
-  console.log(toml.title);
-  console.log(toml.owner.name);
-
-  console.log(yaml.title);
-  console.log(yaml.owner.name);
-
-  console.log(json.title);
-  console.log(json.owner.name);
-})();
 
 (async function testMultiBundle() {
   const btn = document.createElement('button');
