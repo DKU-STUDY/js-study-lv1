42d5270a85a28c2eee773eea9b9430a75dbaa9dc
Seokbin Yoon <nyoongoon@naver.com>
2021-10-25 22:32:16 +0900

    스터디 2주차

--- step2/src/App.js
+++ step2/src/App.js
@@ -1,9 +1,10 @@
 function App(){
-    return '<ul>' +
-        '<li>이름: 윤석빈 </li>'+
-        '<li>나이: 29세</li>'+
-        '<li>회사: 다산에듀</li>'+
-        '</ul>'
+    return
+    `   <ul>
+        <li>이름: 윤석빈 </li>
+        <li>나이: 29세</li>
+        <li>회사: 다산에듀</li>
+        </ul>`
 }
 
 export default App;
